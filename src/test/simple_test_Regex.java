20469d12fd00b4da663ea3f77603237339da1842
谢昊 <799522476@qq.com>
2018-12-15 18:58:18 +0800

    2018.12.15 18.35 update
    注释匹配发现了一个判定问题,并已解决
    文本匹配 ""或者'' 已正常匹配,解决了""中包含\"或''中包含\'的误判(不行了,差点要删库跑路了)
    
    关键字高亮等还没解决
    (have problems with keywords highlight)
    
    test包中 增加了simple_test_Regex类来测试正则表达式是否正常
    TransformTest是来启动文本转化程序的,之前忘说了

--- src/test/simple_test_Regex.java
+++ src/test/simple_test_Regex.java
@@ -7,19 +7,20 @@ import java.io.PrintWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/*java源码转换成HTML*/
+/*此处测试正则表达式匹配是否正确*/
 public class simple_test_Regex {
 
     /**
      * @param args
      */
     public static void main(String[] args) throws Exception {
-        Pattern p = Pattern.compile("\\w");
-        Matcher m = p.matcher("aababbacbadbaeb");
-        if (m.find()) {
+        String input = " \"<style type=\\\"text/css\\\">\",\"1\";\"2\"";
+        System.out.println(input);
+        Pattern p = Pattern.compile("(?<!\\\\)(\\\")(.*?)(?<!\\\\)(\\\")");
+        Matcher m = p.matcher(input);
+        while (m.find()) {
             System.out.println(m.group());
             System.out.println("起点为: "+m.start());
-            System.out.println(m.replaceAll("这里aeb被替换"));
         }
     }
 }
--- src/text_level/JavaSyntaxHighlighter.java
+++ src/text_level/JavaSyntaxHighlighter.java
@@ -47,35 +47,28 @@ public class JavaSyntaxHighlighter {
         String codeline = this.line.substring(0, pos); // 代码部分
         String noteline = this.line.substring(pos); // 不处理行尾注释
 
-        Matcher m = Pattern.compile("((\\\".*?\\\")|(\\'.*?\\'))").matcher(codeline);
-        int n;
-        if (m.find()) {
-            n = m.groupCount();
-            if (n != 0) {
-                String[] strlist = new String[n];
-                for (int i = 0; i < n; i++) {
-                    strlist[i] = m.group();
-                }
-                for (String string : strlist) {
-                    codeline = codeline.replace(string, " [str] " + string + " [end] ");
-                }
-            }
+        //match " 和 '(同时不匹配\"和\'中的"和',防止误判)
+        Matcher m = Pattern.compile("(?<!\\\\)(\\\")(.*?)(?<!\\\\)(\\\")|(?<!\\\\)(\\\')(.*?)(?<!\\\\)(\\\')").matcher(codeline);
+        while (m.find()) {
+            String strlist = m.group();
+            System.out.println("-1 " + " " + strlist);//test
+            codeline = codeline.replace(strlist, " [str] " + strlist + " [end] ");
         }
+        this.line = codeline + noteline;
         //String[] strlist = re.findall("\".*?\"|\'.*?\'", codeline); // 搜索所有字符串
 //        if strlist is not None {
 //            for (String string : strlist) {
 //                codeline = codeline.replace(string, " [str] " + string + " [end] ");
 //            }
 //        }
-        this.line = codeline + noteline;
     }
 
     void highlight_keyword(int pos) {
         //'高亮关键字'
         String codeline = " " + this.line.substring(0, pos) + " ";
         String noteline = this.line.substring(pos);
-        for (int i=0;i<keywords.length;i++) {
-                codeline = codeline.replaceAll(regexkeywords[i], " [key] " + keywords[i] + " [end] ");
+        for (int i = 0; i < keywords.length; i++) {
+            codeline = codeline.replaceAll(regexkeywords[i], " [key] " + keywords[i] + " [end] ");
         }
 //        for r, w in zip (this.regexkeywords, this.keywords){
 //            codeline = re.sub(r, " [key] " + w + " [end] ", codeline);
@@ -115,30 +108,30 @@ public class JavaSyntaxHighlighter {
 
         //用正则模式/(/|\*)(.*)|\*(.*)|(.*)\*/$检查是否为单行注释(//xxx /*xxx*/ *xxx xxx*/)
         //如果是,highlight_note(self.line)高亮注释部分并返回
-        Pattern p1 = Pattern.compile("(/(/|\\*))(.*)|(/\\*(.*))|(/(.*)\\*/)|(^\\*.*$)");
+        Pattern p1 = Pattern.compile("^((/(/|\\*))(.*)|(/\\*(.*))|(/(.*)\\*/)|(^\\*.*$))");
         Matcher find_note1 = p1.matcher(this.line.trim());
         //find_note = re.match('/(/|\*)(.*)|\*(.*)|(.*)\*/$', this.line.trim()); //查找单行注释
         if (find_note1.find()) {
             //处理单行注释;
             note = find_note1.group(0);
             this.highlight_note(note);
-            System.out.println("0 "+note+" ");//test
+            System.out.println("0 " + note + " ");//test
             return this.line;
         }
         int pos = this.line.length();
-        System.out.println("1 "+note+" "+pos);//test
+        System.out.println("1 " + note + " " + pos);//test
 
         //用正则模式(?<=[){};])(.*)/(/|\*).*$查找行尾注释,
         //如果有,返回注释本身note,以及代码与注释的分割位置pos
         //例如:int i = 0; //*Note*, note="//*Note*" pos=10
         //如果没有,note="" pos=len(self.line)分割位置在行尾
         //也就是说:self.line[:pos]为代码,self.line[pos:]为行尾注释
-        Matcher find_note2 = Pattern.compile(".*((?<=[){};])(.*)/(/|\\*).*$)").matcher(this.line.trim());//查找行尾注释
+        Matcher find_note2 = Pattern.compile("((?<=[){};])(.*)/(/|\\*).*$)").matcher(this.line.trim());//查找行尾注释
 
         if (find_note2.find()) {
-            note = find_note2.group(1);  //标记行尾注释;
-            pos = find_note2.start(1);//标记注释位置;//span() 返回一个元组包含匹配 (开始,结束) 的位置
-            System.out.println("2 "+note+" "+pos);//test
+            note = find_note2.group();  //标记行尾注释;
+            pos = find_note2.start();//标记注释位置;//span() 返回一个元组包含匹配 (开始,结束) 的位置
+            System.out.println("2 " + note + " " + pos);//test
         }
 
         this.highlight_note(note);  //处理行尾注释;
